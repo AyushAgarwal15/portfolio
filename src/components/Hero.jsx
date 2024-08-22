8189764938c32f0ceffd2032d0897320f6cf66c8
Ayush Agarwal <ayushagarwal8126@gmail.com>
2024-08-22 20:29:53 +0530

    added a model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { PlanetCanvas } from "./canvas";
+import { ComputerCanvas, PlanetCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <PlanetCanvas />
+      <ComputerCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,5 +2,6 @@ import MenCanvas from "./Men";
 import BallCanvas from "./Ball";
 import StarsCanvas from "./Stars";
 import PlanetCanvas from "./planet";
+import ComputerCanvas from "./Computers";
 
-export { MenCanvas, BallCanvas, PlanetCanvas, StarsCanvas };
+export { MenCanvas, BallCanvas, PlanetCanvas, StarsCanvas, ComputerCanvas };
