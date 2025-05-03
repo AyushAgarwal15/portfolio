f75c1af619d815f3bb89cb2fdc642b21a4fea502
Ayush Agarwal <ayushagarwal8126@gmail.com>
2025-05-03 14:07:54 +0530

    Update Navbar component with new Google Drive links for shared files.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
               <a
                 key={nav.id}
                 target="_blank"
-                href="https://drive.google.com/file/d/1P8SsQ56zW0tMIHOHHhELdJD0ZjXAMOzL/view?usp=sharing"
+                href="https://drive.google.com/file/d/1RuAvE2iuvTGbW9M80OOKPjNAzh_NmdAL/view?usp=sharing"
                 className={
                   "text-secondary hover:text-white text-[18px] font-medium cursor-pointer"
                 }
@@ -94,7 +94,7 @@ const Navbar = () => {
                   <a
                     key={nav.id}
                     target="_blank"
-                    href="https://drive.google.com/file/d/1P8SsQ56zW0tMIHOHHhELdJD0ZjXAMOzL/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1RuAvE2iuvTGbW9M80OOKPjNAzh_NmdAL/view?usp=sharing"
                     className={
                       "text-secondary hover:text-white text-[18px] font-medium cursor-pointer"
                     }
